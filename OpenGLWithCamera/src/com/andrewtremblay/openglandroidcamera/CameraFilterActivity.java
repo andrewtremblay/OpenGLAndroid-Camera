42bc23e1e3fb634acaca66daccefef5494c68927
AndrewTremblay <gloriousrocketship@gmail.com>
2014-04-30 16:19:40 -0700

    CameraFilterActivity now requests fullscreen layout

--- OpenGLWithCamera/src/com/andrewtremblay/openglandroidcamera/CameraFilterActivity.java
+++ OpenGLWithCamera/src/com/andrewtremblay/openglandroidcamera/CameraFilterActivity.java
@@ -3,24 +3,29 @@
  */
 package com.andrewtremblay.openglandroidcamera;
 
-import com.officialandroidexample.gl2cameraeye.CamGLSurfaceView;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
- * @author andrewtremblay
+ *  An example full-screen activity that shows the default camera through a CamGLSurfaceView.
+ *  @author andrewtremblay
  *
  */
 public class CameraFilterActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,  WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         mGLView = new CamGLSurfaceView(this);
         setContentView(mGLView);
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 
     @Override
